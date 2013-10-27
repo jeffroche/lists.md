f574631bd06859f4c4925976b74f93bdfed9d479
jeffroche <jeff.roche@gmail.com>
2013-10-27 16:14:34 -0400

    Prepping heroku for redis

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ from dropbox.rest import ErrorResponse
 from markdown2 import MarkdownError
 import redis
 from datetime import datetime as dt
+import json
 
 
 HEROKU = os.environ.get('HEROKU', False)
@@ -16,6 +17,9 @@ if not HEROKU:
     r = redis.StrictRedis(host='localhost',
                           port=6379,
                           db=0)
+else:
+    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+    r = redis.from_url(redis_url)
 
 DROPBOX_ACCESS_TOKEN = os.environ.get('DROPBOX_TOKEN')
 DROPBOX_LIST_FOLDER = os.environ.get('DROPBOX_LIST_FOLDER')
@@ -29,16 +33,22 @@ db_client = listsdotmd.connect_to_dropbox(
 
 @app.route('/')
 def list_lists():
-    folder_info = r.get('index', None)
+    r_index = r.get('index')
+    if r_index:
+        folder_info = json.loads(r_index)
+        print "Index loaded from Redis"
+    else:
+        folder_info = None
     folder_update_time = listsdotmd.last_update_time(db_client,
         DROPBOX_LIST_FOLDER)
     if folder_info and (
-        dt.strptime(folder_info.last_update, date_fmt) >= dt.strptime(folder_update_time, date_fmt)):
-        all_lists = folder_info.data
+        dt.strptime(folder_info['last_update'], date_fmt) >= dt.strptime(folder_update_time, date_fmt)):
+        all_lists = folder_info['data']
     else:
         all_lists = listsdotmd.index_lists(db_client,
             DROPBOX_LIST_FOLDER)
-        r.set('index', {'data': all_lists, 'last_update': folder_update_time})
+        r.set('index', json.dumps(
+            {'data': all_lists, 'last_update': folder_update_time}))
     return render_template('index.html', lists=all_lists)
 
 
@@ -46,20 +56,26 @@ def list_lists():
 def show_list(list_name):
     list_path = os.path.join(DROPBOX_LIST_FOLDER,
         list_name + '.md')
-    list_info = r.get(list_name, None)
+    r_list = r.get(list_name)
+    if r_list:
+        list_info = json.loads(r_list)
+        print "List loaded from Redis"
+    else:
+        list_info = None
     try:
         list_update_time = listsdotmd.last_update_time(db_client, list_path)
     except ErrorResponse:
         abort(404)
     if list_info and (
-        dt.strptime(list_info.last_update, date_fmt) >= dt.strptime(list_update_time, date_fmt)):
-        list_str = list_info.data
+        dt.strptime(list_info['last_update'], date_fmt) >= dt.strptime(list_update_time, date_fmt)):
+        list_str = list_info['data']
     else:
         try:
             list_str = listsdotmd.load_list_as_string(db_client, list_path)
         except ErrorResponse:
             abort(404)
-        r.set(list_name, {'data': list_str, 'last_update': list_update_time})
+        r.set(list_name, json.dumps(
+            {'data': list_str, 'last_update': list_update_time}))
     try:
         list_html = listsdotmd.convert_list_to_html(list_str)
     except MarkdownError:
